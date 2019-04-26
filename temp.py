9352923bc51ab9bcfb107059ed9a02606c82915b
yanqing0709 <smile870709@yahoo.com.tw>
2019-04-26 13:34:34 +0800

    newfile temp.py

---
+++ temp.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#master
+
+#示範Git用的程式碼！
+
+def add(a, b):
+    return a + b
+
+def minus(a, b):
+    return a - b
+
+def times(a, b):
+    return a * b
+
+def divided(a, b):
+    if b != 0:
+        return a / b
+    return "無限"
